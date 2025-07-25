98808ad6dc5483e9d444fd3eb1b4393293c9c849
vhenriquezn <32468931+vhenriquezn@users.noreply.github.com>
2025-07-24 23:33:44 -0400

    Update buy.py

--- pyquotex/ws/channels/buy.py
+++ pyquotex/ws/channels/buy.py
@@ -48,5 +48,5 @@ class Buy(Base):
         self.send_websocket_request(data)
 
         data = f'42["orders/open",{json.dumps(payload)}]'
-        print(data)
+        #print(data)
         self.send_websocket_request(data)
