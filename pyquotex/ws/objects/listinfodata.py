33d194a24a33aca9a4f735e73b09c83559530e3e
vhenriquezn <32468931+vhenriquezn@users.noreply.github.com>
2025-07-29 20:03:48 -0400

    Update listinfodata.py

--- pyquotex/ws/objects/listinfodata.py
+++ pyquotex/ws/objects/listinfodata.py
@@ -11,9 +11,8 @@ class ListInfoData(Base):
         self.__name = "listInfoData"
         self.listinfodata_dict = {}
 
-    def set(self, result, game_state, percent_profit, id_number):
+    def set(self, game_state, percent_profit, id_number):
         self.listinfodata_dict[id_number] = {
-            "result": result,
             "game_state": game_state,
             "percent_profit": percent_profit
         }
