e85aa86d37638e302ea578353911f74b0226b646
vhenriquezn <32468931+vhenriquezn@users.noreply.github.com>
2025-08-14 21:33:22 -0400

    actualizacion carpeta pyq

--- pyquotex/api.py
+++ pyquotex/api.py
@@ -216,7 +216,6 @@ class QuotexAPI(object):
             "tournamentId": self.tournament_id
         }
         data = f'42["account/change",{json.dumps(payload)}]'
-        print(f"{data}\n\n\n\n\n\n")
         self.send_websocket_request(data)
 
     def get_history_line(self, asset_id, index, end_from_time, offset):
